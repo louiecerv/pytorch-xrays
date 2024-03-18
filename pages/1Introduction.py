88e792fa3ff761ffc9d199099d79ea2f93fc30a0
Louie F. Cervantes <lfcervantes@wvsu.edu.ph>
2024-03-18 07:57:38 +0000

    Save

--- pages/1Introduction.py
+++ pages/1Introduction.py
@@ -163,12 +163,12 @@ def test(model, device, test_loader):
     correct = 0
     with torch.no_grad():
         for data, target in test_loader:
-        data, target = data.to(device), target.to(device)
-        output = model(data)
-        test_loss += F.nll_loss(output, target,
-        reduction='sum').item()
-        pred = output.argmax(dim=1, keepdim=True)
-        correct += pred.eq(target.view_as(pred)).sum().item()
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            test_loss += F.nll_loss(output, target,
+            reduction='sum').item()
+            pred = output.argmax(dim=1, keepdim=True)
+            correct += pred.eq(target.view_as(pred)).sum().item()
 
     test_loss /= len(test_loader.dataset)
     test_losses.append(test_loss)
