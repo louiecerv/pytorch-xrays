487addd7464914ec4f725e08edfc8209fa99ea32
Louie F. Cervantes <lfcervantes@wvsu.edu.ph>
2024-03-18 06:21:14 +0000

    SAve

--- pages/1Introduction.py
+++ pages/1Introduction.py
@@ -81,7 +81,7 @@ def app():
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
 
-    train_data = datasets.ImageFolder(os.path.join(data_path, 'train_set'), transform= train_transform)
+    train_data = datasets.ImageFolder(os.path.join(data_path, 'training_set'), transform= train_transform)
     test_data = datasets.ImageFolder(os.path.join(data_path, 'test_set'), transform= test_transform)
 
     train_loader = DataLoader(train_data, batch_size= 16, shuffle= True, pin_memory= True)
